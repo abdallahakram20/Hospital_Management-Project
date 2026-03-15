1eae0df9ab1f49fed9ff89c535c5ad95504a02a0
Abdallah <Abdallah@DESKTOP-UFO9921>
2026-03-15 23:56:21 +0200

    feat(Patient): add data annotations and validation attributes

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -1,6 +1,29 @@
-﻿namespace Hospital_Management_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hospital_Management_Project.Models
 {
     public class Patient
     {
+        [Key]
+        [MaxLength(20)]
+        public string patientId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string FName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LName { get; set; }
+
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+        [MaxLength(20)]
+        public string Phone { get; set; }
+         public enum Gender
+        {
+            Male,
+            Female
+        }
     }
 }
