1156281710197fecb41020cfafbf17c5bdef1e78
DESKTOP <DESKTOP@DESKTOP-6LJ5PQF>
2026-03-15 23:46:31 +0200

    feat: add solution file and Appointment model skeleton

---
+++ Models/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hospital_Management_Project.Models
+{
+    public class Appointment
+    {
+        public string Appointment_ID { get; set; }
+
+        [Required]
+        public string Patient_ID { get; set; }
+
+        [Required]
+        public string Staff_ID { get; set; }
+
+        [Required]
+        public DateTime Appointment_Date { get; set; }
+        public string Status { get; set; }  
+        public string Reason { get; set; }
+    }
+}
