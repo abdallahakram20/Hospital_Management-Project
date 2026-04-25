eb67a8a366db59a293748340c4a7b7dc4787ba12
Hassan Masoud <hassanmasoud2023@gmail.com>
2026-04-25 23:02:33 +0300

    refactor(AppDbContext): remove commented-out relationship configurations

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hospital_Management_Project.Models
 {
@@ -26,34 +26,6 @@ namespace Hospital_Management_Project.Models
             base.OnModelCreating(modelBuilder);
 
 
-            // Relationship between Patient and Appointment (1 Patient To Many Appointment)
-            modelBuilder.Entity<Patient>()
-                .HasOne(p => p.Appointment) // Patient has Many Appointments
-                .WithOne(a => a.Patient) // Appointment has One Patients
-                .HasForeignKey<Appointment>(a => a.PatientID); // Foreign Key in Appointment
-
-
-
-            // Relationship between Staff and Appointment (1 Staff To Many Appointment)
-            modelBuilder.Entity<Staff>()
-                .HasMany(s => s.Appointments) // Patient has Many Appointments
-                .WithOne(a => a.Staff) // Appointment has One Patients
-                .HasForeignKey(a => a.StaffID); // Foreign Key in Appointment
-
-
-            //Relationship Between STAFF and DEPARTMENT (  ONE    DEPARTMENT   TO  MANY  STAFF  )
-            modelBuilder.Entity<Department>()
-                .HasMany(d => d.Staffs)
-                .WithOne(s => s.Department)
-                .HasForeignKey(s => s.DeptID);
-
-
-            // Relation Between  Pateint & Pateint_Medical_Profile  (1  Pateint One Patient_Medical_Profile)
-
-            modelBuilder.Entity<Patient>()
-                .HasOne(p => p.Patient_Medical_Profile)
-                .WithOne(mp => mp.Patient)
-                .HasForeignKey<Patient_Medical_Profile>(mp => mp.PatientID);
         }
     }
 }
