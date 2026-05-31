e0f751a1b022a5feeca24434865c979ea4091346
520menna <mennak0688@gmail.com>
2026-05-31 14:43:47 +0300

    feat(ui): add hospital logo and configure initial styling

---
+++ Migrations/20260530115534_hospital.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital_Management_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class hospital : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
